7b79c09f05f03c04192ddd6cf97027674954be1d
MSK4477 <ks7997067@gmail.com>
2023-11-02 20:25:06 +0530

    completed

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoSlice from "../Redux/slices/todoSlice";
-const storeToLocalStoreage = ({getState}) => next => action => {
-    const result = next(action)
-    localStorage.setItem("reduxState", JSON.stringify(getState()))
-
-    return result 
-    
-}
+const storeToLocalStoreage = ({ getState }) => (next) => (action) => {
+    const result = next(action);
+  
+    try {
+      localStorage.setItem("reduxState", JSON.stringify(getState()));
+    } catch (error) {
+      console.error("Error saving Redux state to local storage:", error);
+    }
+  
+    return result;
+  };
+  
 const store  = configureStore ({
     reducer: {
         todo:todoSlice
 
     },
+
     middleware:( getDefaultMiddleware )=> getDefaultMiddleware().concat(storeToLocalStoreage)
 
 })
